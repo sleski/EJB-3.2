c2b0cddf9f0f232ee708f99a003fb3a00ae73d14
Slawomir Leski <s.leski@e-media.de>
2014-02-11 15:42:02 +0100

    Added new subproject - kickstart'er

---
+++ KickStart/README.md
@@ -0,0 +1,10 @@
+Let's start EJB 3.2!
+===
+
+Kick Start:
+
+1)configure
+
+2)build
+
+3)deploy
---
+++ KickStart/src/test/java/it/tostao/kickstart/SimpleTest.java
@@ -0,0 +1,13 @@
+package it.tostao.kickstart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
